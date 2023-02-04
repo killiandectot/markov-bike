e170febe5d86f0381065acad52a0dbc82c383ef6
juan garassino <juan.garassino@gmail.com>
2023-02-04 17:47:06 +0100

    worked a bit in the data sourcing and added somethings on the main.py

--- markovBike/main.py
+++ markovBike/main.py
@@ -1,17 +1,24 @@
-from markovBike.data_source.source import database_queries, get_stations_data, get_trips_data
-from markovBike.data_source.preprocess import preprocess_stations_data, preprocess_trips_data
+from markovBike.data_source.source import (
+    database_queries,
+    get_stations_data,
+    get_trips_data,
+)
+from markovBike.data_source.preprocess import (
+    preprocess_stations_data,
+    preprocess_trips_data,
+)
 
 from markovBike.forecast.forecast import forecast_number_users
 
 queries = database_queries(12)
 
-stations_raw_dataframe = get_stations_data(queries['stations'])
+stations_raw_dataframe = get_stations_data(queries["stations"])
 
-trips_raw_dataframe = get_trips_data(queries['trips'])
+trips_raw_dataframe = get_trips_data(queries["trips"])
 
-stations_preproc_dataframe = preprocess_stations_data(stations_raw_dataframe) # TO DO
+stations_preproc_dataframe = preprocess_stations_data(stations_raw_dataframe)  # TO DO
 
-trips_preproc_dataframe = preprocess_trips_data(trips_raw_dataframe) # TO DO
+trips_preproc_dataframe = preprocess_trips_data(trips_raw_dataframe)  # TO DO
 
 number_of_users = forecast_number_users()
 
